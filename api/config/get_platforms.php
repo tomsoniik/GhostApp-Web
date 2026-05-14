0fc786945296259474382e80447197841c33e850
tomson <tomson.baron@onet.pl>
2026-05-15 00:46:28 +0200

    fix: corrected relative path for Database import in get_platforms.php

--- api/config/get_platforms.php
+++ api/config/get_platforms.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/Database.php';
 
 $database = new Database();
 $db = $database->getConnection();
